73cddbb6337ceb338b7e4b2ad56490a709c44a7c
Saurabh Kumar <saurabhkumar@Saurabhs-MacBook-Pro.local>
2023-01-21 15:14:16 +0530

    feat: initial api commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const qr = require("qr-image");
+
+const app = express();
+
+app.get("/qr", (req, res) => {
+  const url = req.query.url;
+  const qr_svg = qr.image(url, { type: "svg" });
+  res.setHeader("Content-disposition", "attachment; filename=qr.svg");
+  res.setHeader("Content-type", "image/svg+xml");
+  qr_svg.pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log("QR code generator listening on port 3000!");
+});
